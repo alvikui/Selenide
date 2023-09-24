ef39c52347ed6cad65467ac8bd5463bd7d1ac313
Alena Kuimova <alvikui@yandex.ru>
2023-09-24 16:36:19 +0600

    Change test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -18,7 +18,7 @@ public class CardDeliveryTest {
     public void shouldBeCompletedSuccessfully() {
         open("http://localhost:9999");
         $("[data-test-id = 'city'] input").setValue("Москва");
-        String planningDate = generateDate(3, "dd.MM.yyyy");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id = 'date'] input").setValue(planningDate);
         $("[data-test-id = 'name'] input").setValue("Морковкина Мария");
